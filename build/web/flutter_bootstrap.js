55697057862fb41a4f004288e9bde3cf2de7caaf
Rayan <rayannnkhan0@gmail.com>
2025-08-15 04:57:05 -0700

    Initial commit for Around_Fast with build/web included

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"ef0cd000916d64fa0c5d09cc809fa7ad244a57
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1753329752"
+    serviceWorkerVersion: "1964482607"
   }
 });
